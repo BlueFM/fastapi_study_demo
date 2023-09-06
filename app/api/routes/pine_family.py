f65e4f2ee153f83d7af3af15c121e5d98e439d3e
van <vanblue2023@gmail.com>
2023-09-06 14:23:50 +0800

    进行封装

--- app/api/routes/pine_family.py
+++ app/api/routes/pine_family.py
@@ -7,14 +7,7 @@ from app import db_client
 router = APIRouter()
 
 
-@router.post("/pinefamilyinfo", response_model=PineFamilyResponse, name="pine_family:pinefamilyinfo")
-async def pinefamilyinfo(data_input: PineFamilyDataInput):
-    member = PineFamily(name=data_input.name, age=data_input.age, sex=data_input.sex, profession=data_input.profession,
-                        hobby=data_input.hobby,
-                        motto=data_input.motto)
-    await db_client.insert(member)
-    sql = select(PineFamily.name, PineFamily.sex, PineFamily.age, PineFamily.profession, PineFamily.hobby,
-                 PineFamily.motto).where(PineFamily.name == data_input.name)
+async def ret(sql):
     resp = await db_client.select(sql)
     row = resp.fetchone()
     member_dict = {
@@ -27,21 +20,23 @@ async def pinefamilyinfo(data_input: PineFamilyDataInput):
     }
     data_info = {"code": 200, "data": member_dict}
     return PineFamilyResponse.parse_obj(data_info)
+
+
+@router.post("/pinefamilyinfo", response_model=PineFamilyResponse, name="pine_family:pinefamilyinfo")
+async def pinefamilyinfo(data_input: PineFamilyDataInput):
+    member = PineFamily(name=data_input.name, age=data_input.age, sex=data_input.sex, profession=data_input.profession,
+                        hobby=data_input.hobby,
+                        motto=data_input.motto)
+    # 插入之前先校验数据库中是否已经存在该成员
+
+    await db_client.insert(member)
+    sql = select(PineFamily.name, PineFamily.sex, PineFamily.age, PineFamily.profession, PineFamily.hobby,
+                 PineFamily.motto).where(PineFamily.name == data_input.name)
+    return await ret(sql)
 
 
 @router.get("/pinefamilyinfo/getname", response_model=PineFamilyResponse)
 async def get_info(input_name: str):
     sql = select(PineFamily.name, PineFamily.sex, PineFamily.profession, PineFamily.hobby, PineFamily.motto).where(
         PineFamily.name == input_name)
-    resp = await db_client.select(sql)
-    row = resp.fetchone()
-    member_dict = {
-        "name": row[0],
-        "sex": row[1],
-        "age": row[2],
-        "profession": row[3],
-        "hobby": row[4],
-        "motto": row[5]
-    }
-    data_info = {"code": 200, "data": member_dict}
-    return PineFamilyResponse.parse_obj(data_info)
+    return await ret(sql)
